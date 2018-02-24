23ed1ad3ec7802c175f026a6d07ef9e429c56895
Jake Engel <jakeengel01@gmail.com>
2018-02-24 08:32:14 -0500

    Emit newMessage on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server!');
-
-  socket.emit('createMessage', {
-    from: 'jake2@jake.com',
-    text: 'Hey Jake, it\'s Jake#2'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.emit('newMessage', {
-    from: 'Jake#1',
-    text: 'Hey Jake it\'s Jake#1!',
-    createdAt: '123456'
-  });
-
   socket.on('createMessage', (message) => {
     console.log('newMessage: ', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
